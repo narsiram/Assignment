12f60dbd6736b2b1eca88d983387f50a5e304af3
Narsi JINDAL <narsi@churchtalk.io>
2020-11-07 21:41:59 +0530

    insertion

--- app/src/main/java/com/sumcorp/shaadiassignment/data/local/dao/ShaadiDao.kt
+++ app/src/main/java/com/sumcorp/shaadiassignment/data/local/dao/ShaadiDao.kt
@@ -2,11 +2,13 @@ package com.sumcorp.shaadiassignment.data.local.dao
 
 import androidx.room.*
 import com.sumcorp.shaadiassignment.data.local.entity.model.ResultData
+import com.sumcorp.shaadiassignment.data.local.entity.model.ShaadiRequestResponse
 
 @Dao
 interface ShaadiDao {
 
-    @Insert
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertData(resultData: ResultData)
 
     @Query("select * from tbl_shaadi_data")
